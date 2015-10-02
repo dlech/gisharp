72fcda22e169767173518256480071e19127b37d
David Lechner <david@lechnology.com>
2015-10-02 16:41:42 -0500

    Add tests for DateDay and DateYear

---
+++ GLib.Test/DateDayTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GISharp.GLib.Test
+{
+    [TestFixture]
+    public class DateDayTests
+    {
+        [Test]
+        public void TestImplicitOperator ()
+        {
+            byte expected = 1;
+            DateDay dateDay = expected;
+            byte actual = dateDay;
+            Assert.That (actual, Is.EqualTo (expected));
+        }
+    }
+}
---
+++ GLib.Test/DateYearTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GISharp.GLib.Test
+{
+    [TestFixture]
+    public class DateYearTests
+    {
+        [Test]
+        public void TestImplicitOperator ()
+        {
+            ushort expected = 1;
+            DateYear dateYear = expected;
+            ushort actual = dateYear;
+            Assert.That (actual, Is.EqualTo (expected));
+        }
+    }
+}
