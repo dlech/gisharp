ab97881fe67a627bba2a94571e65577e5fae08e7
David Lechner <david@lechnology.com>
2015-10-07 18:37:05 -0500

    Add BinaryAge and InterfaceAge properties to GLib.Version

--- GLib-2.0/Version.cs
+++ GLib-2.0/Version.cs
@@ -7,7 +7,7 @@ namespace GISharp.GLib
     public static partial class Version
     {
         /// <summary>
-        /// Gets the GLib version that was used at compile time.
+        /// The version of the GLib library that was used at compile time.
         /// </summary>
         /// <value>The compile time version.</value>
         public static System.Version CompileTime {
@@ -25,9 +25,53 @@ namespace GISharp.GLib
             }
         });
 
+        /// <summary>
+        /// The version of the GLib library linked against at run time.
+        /// </summary>
+        /// <value>The run time.</value>
         public static System.Version RunTime {
             get {
                 return _RunTime.Value;
+            }
+        }
+
+        static Lazy<int> _BinaryAge = new Lazy<int> (() => {
+            using (var lib = new DynamicLibrary ("glib-2.0")) {
+                return lib.GetInt32 ("glib_binary_age").Value;
+            }
+        });
+
+        /// <summary>
+        /// Defines how far back backwards compatibility reaches.
+        /// </summary>
+        /// <value>The binary age of the GLib library.</value>
+        /// <remarks>
+        /// An integer variable exported from the library linked against at
+        /// application run time.
+        /// </remarks>
+        public static int BinaryAge {
+            get {
+                return _BinaryAge.Value;
+            }
+        }
+
+        static Lazy<int> _InterfaceAge = new Lazy<int> (() => {
+            using (var lib = new DynamicLibrary ("glib-2.0")) {
+                return lib.GetInt32 ("glib_interface_age").Value;
+            }
+        });
+
+        /// <summary>
+        /// Defines how far back the API has last been extended.
+        /// </summary>
+        /// <value>The interface age of the GLib library.</value>
+        /// <remarks>
+        /// An integer variable exported from the library linked against at
+        /// application run time.
+        /// </remarks>
+        public static int InterfaceAge {
+            get {
+                return _InterfaceAge.Value;
             }
         }
     }
