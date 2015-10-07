8de8133520b69e686fb929f6c7dac52c917eecc2
David Lechner <david@lechnology.com>
2015-10-07 17:47:45 -0500

    Add DynamicLibrary class to Core
    This class is uses to read symbols from dynamic libraries. It is needed since pinvoke only works with functions.

---
+++ Core/DynamicLibrary.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GISharp.Core
+{
+    public class DynamicLibrary : Opaque
+    {
+        const string LibraryPrefix = "lib";
+        const string LibrarySuffix = ".dylib";
+
+        static object lockObj = new object ();
+
+        [DllImport ("ld.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr dlopen (string path, Mode mode);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DynamicLibrary"/> class.
+        /// </summary>
+        /// <param name="name">The name of the library.</param>
+        /// <param name="mode">The mode flags.</param>
+        public DynamicLibrary (string name, Mode mode = Mode.Lazy)
+        {
+            if (name == null) {
+                throw new ArgumentNullException (nameof(name));
+            }
+            if (!mode.HasFlag (Mode.Lazy) && !mode.HasFlag (Mode.Now)) {
+                var message = "mode must contain one of Mode.Lazy or Mode.Now";
+                throw new ArgumentException (message, nameof(mode));
+            }
+
+            // TODO: need to make this platform-specific
+            name = string.Format ($"{LibraryPrefix}{name}{LibrarySuffix}");
+
+            lock (lockObj) {
+                Handle = dlopen (name, mode);
+                if (Handle == IntPtr.Zero) {
+                    throw new Exception (CheckLastError ());
+                }
+            }
+        }
+
+        public int? GetInt32 (string symbol)
+        {
+            AssertNotDisposed ();
+            var ptr = GetSymbol (symbol);
+            if (ptr == IntPtr.Zero) {
+                return null;
+            }
+            return Marshal.ReadInt32 (ptr);
+        }
+
+        public uint? GetUInt32 (string symbol)
+        {
+            AssertNotDisposed ();
+            var ptr = GetSymbol (symbol);
+            if (ptr == IntPtr.Zero) {
+                return null;
+            }
+            return(uint)Marshal.ReadInt32 (ptr);
+        }
+
+        [DllImport ("ld.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr dlsym (IntPtr handle, string symbol);
+
+        IntPtr GetSymbol (string symbol)
+        {
+            if (symbol == null) {
+                throw new ArgumentNullException (nameof(symbol));
+            }
+            lock (lockObj) {
+                dlerror (); // clear existing error
+                var ret = dlsym (Handle, symbol);
+                var err = CheckLastError (); 
+                if (err!= null) {
+                    throw new Exception (err);
+                }
+                return ret;
+            }
+        }
+
+        [DllImport ("ld.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int dlclose (IntPtr handle);
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!IsDisposed) {
+                dlclose (Handle);
+            }
+            base.Dispose(disposing);
+        }
+
+        [DllImport ("ld.dll")]
+        internal static extern IntPtr dlerror ();
+
+        static string CheckLastError ()
+        {
+            // we can't free the string returned from dlerror
+            return Marshal.PtrToStringAnsi (dlerror ());
+        }
+
+        [Flags]
+        public enum Mode
+        {
+            /// <summary>
+            /// Lazy function call binding.
+            /// </summary>
+            Lazy      = 0x00001,
+
+            /// <summary>
+            /// Immediate function call binding.
+            /// </summary>
+            Now       = 0x00002,
+
+            /// <summary>
+            /// Mask of binding time value.
+            /// </summary>
+            BindingMask = 0x3,
+
+            /// <summary>
+            /// Do not load the object.
+            /// </summary>
+            NoLoad    = 0x00004,
+
+            /// <summary>
+            /// Use deep binding.
+            /// </summary>
+            DeepBind  = 0x00008,
+
+            /// <summary>
+            /// The symbols of the loaded object and its dependencies are made
+            /// visible as if the object were linked directly into the program.
+            /// </summary>
+            Global =    0x00100,
+
+            /// <summary>
+            /// Inverse of <see cref="Global"/>. (Default)
+            /// </summary>
+            Local = 0,
+
+            /// <summary>>
+            /// Do not delete object when closed.
+            /// </summary>
+            NoDelete =  0x01000,
+        }
+    }
+}
