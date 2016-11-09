74002504f978af6fb306c7706e5d74fbe3a25d39
David Lechner <david@lechnology.com>
2016-11-09 16:00:22 -0600

    Cleanup and expand Timeout tests

--- Core.Test/GLib/TimeoutTests.cs
+++ Core.Test/GLib/TimeoutTests.cs
@@ -1,8 +1,6 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Threading.Tasks;
-
-using GISharp.GLib;
+using System;
 
 namespace GISharp.GLib.Test
 {
@@ -12,19 +10,29 @@ namespace GISharp.GLib.Test
         [Test]
         public void TestAdd ()
         {
+            // null function is not allowed
+            Assert.That (() => Timeout.Add (0, null),
+                Throws.InstanceOf<ArgumentNullException> ());
+
+            // Timeout.Add() can only attach sources to the global main context,
+            // so we need lock to ensure we have exclusive use.
             lock (MainContextTests.MainContextLock) {
                 var timeoutInvoked = false;
+                var mainLoop = new MainLoop ();
 
+                var id = Timeout.Add (0, () => {
+                    mainLoop.Quit ();
+                    timeoutInvoked = true;
+                    return Source.Remove_;
+                });
+
+                Assert.That (id, Is.Not.EqualTo (0));
+
+                var source = MainContext.Default.FindSourceById (id);
                 Task.Run (() => {
-                    var mainLoop = new MainLoop ();
-                    var id = Timeout.Add (0, () => {
-                        mainLoop.Quit ();
-                        timeoutInvoked = true;
-                        return Source.Remove_;
-                    });
-                    Assert.That (id, Is.Not.EqualTo (0));
                     mainLoop.Run ();
                 }).Wait (100);
+                source.Destroy ();
 
                 Assert.That (timeoutInvoked, Is.True);
             }
@@ -33,19 +41,29 @@ namespace GISharp.GLib.Test
         [Test]
         public void TestAddSeconds ()
         {
+            // null function is not allowed
+            Assert.That (() => Timeout.AddSeconds (0, null),
+                Throws.InstanceOf<ArgumentNullException> ());
+            
+            // Timeout.AddSeconds() can only attach sources to the global main
+            // context, so we need lock to ensure we have exclusive use.
             lock (MainContextTests.MainContextLock) {
                 var timeoutInvoked = false;
+                var mainLoop = new MainLoop ();
 
+                var id = Timeout.AddSeconds (0, () => {
+                    mainLoop.Quit ();
+                    timeoutInvoked = true;
+                    return Source.Remove_;
+                });
+
+                Assert.That (id, Is.Not.EqualTo (0));
+
+                var source = MainContext.Default.FindSourceById (id);
                 Task.Run (() => {
-                    var mainLoop = new MainLoop ();
-                    var id = Timeout.AddSeconds (0, () => {
-                        mainLoop.Quit ();
-                        timeoutInvoked = true;
-                        return Source.Remove_;
-                    });
-                    Assert.That (id, Is.Not.EqualTo (0));
                     mainLoop.Run ();
                 }).Wait (2000);
+                source.Destroy ();
 
                 Assert.That (timeoutInvoked, Is.True);
             }
@@ -54,15 +72,55 @@ namespace GISharp.GLib.Test
         [Test]
         public void TestCreateSource ()
         {
+            var timeoutInvoked = false;
             var source = Timeout.CreateSource (0);
             Assert.That (source, Is.Not.Null);
+
+            var context = new MainContext ();
+            var mainLoop = new MainLoop (context);
+
+            source.SetCallback (() => {
+                mainLoop.Quit ();
+                timeoutInvoked = true;
+                return Source.Remove_;
+            });
+
+            source.Attach (context);
+            Task.Run (() => {
+                context.PushThreadDefault ();
+                mainLoop.Run ();
+                context.PopThreadDefault ();
+            }).Wait (100);
+            source.Destroy ();
+
+            Assert.That (timeoutInvoked, Is.True);
         }
 
         [Test]
         public void TestCreateSourceSeconds ()
         {
+            var timeoutInvoked = false;
             var source = Timeout.CreateSourceSeconds (0);
             Assert.That (source, Is.Not.Null);
+
+            var context = new MainContext ();
+            var mainLoop = new MainLoop (context);
+
+            source.SetCallback (() => {
+                mainLoop.Quit ();
+                timeoutInvoked = true;
+                return Source.Remove_;
+            });
+
+            source.Attach (context);
+            Task.Run (() => {
+                context.PushThreadDefault ();
+                mainLoop.Run ();
+                context.PopThreadDefault ();
+            }).Wait (2000);
+            source.Destroy ();
+
+            Assert.That (timeoutInvoked, Is.True);
         }
     }
 }
