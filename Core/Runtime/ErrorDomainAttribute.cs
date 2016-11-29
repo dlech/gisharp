5e27ad1583aefcc7edc6a13719b875f3ed42bd7f
David Lechner <david@lechnology.com>
2016-11-28 23:29:17 -0600

    Add some docs on ErrorDomainAttribute

--- Core/Runtime/ErrorDomainAttribute.cs
+++ Core/Runtime/ErrorDomainAttribute.cs
@@ -4,19 +4,45 @@ using GISharp.GLib;
 
 namespace GISharp.Runtime
 {
+    /// <summary>
+    /// Indicates that this Enum contains error codes for an error domain.
+    /// </summary>
     [AttributeUsage (AttributeTargets.Enum)]
     public sealed class ErrorDomainAttribute : Attribute
     {
+        /// <summary>
+        /// Gets the name of the error domain.
+        /// </summary>
+        /// <value>The name of the error domain.</value>
         public string ErrorDomain { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorDomainAttribute"/> class.
+        /// </summary>
+        /// <param name="errorDomain">The name of the error domain.</param>
+        /// <remarks>
+        /// The name must match the error domain quark for this error domain.
+        /// </remarks>
         public ErrorDomainAttribute (string errorDomain)
         {
             ErrorDomain = errorDomain;
         }
     }
 
+    /// <summary>
+    /// Extension methods related to <see cref="ErrorDomainAttribute"/>.
+    /// </summary>
     public static class ErrorDomainAttributeExtensions
     {
+        /// <summary>
+        /// Gets the error domain of an Enum.
+        /// </summary>
+        /// <returns>The error domain.</returns>
+        /// <param name="value">Value.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown if type of <paramref name="value"/> is not decorated with
+        /// <see cref="ErrorDomainAttribute"/>.
+        /// </exception>
         public static Quark GetErrorDomain (this Enum value)
         {
             var type = value.GetType ();
