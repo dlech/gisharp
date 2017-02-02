b4672d9baca30e4319b7088da660af0820468ab1
David Lechner <david@lechnology.com>
2017-02-02 17:47:55 -0600

    Correctly setup entry point for Core.Test

--- Core.Test/Program.cs
+++ Core.Test/Program.cs
@@ -7,7 +7,7 @@ namespace Core.Test
 {
     public class Program
     {
-        public int Main (string[] args)
+        public static int Main (string[] args)
         {
             return new AutoRun(typeof(Program).GetTypeInfo().Assembly)
                 .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
