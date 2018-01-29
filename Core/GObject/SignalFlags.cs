8c3040f2de847c65750193fda90a0949fdc52739
David Lechner <david@lechnology.com>
2018-01-29 13:15:07 -0600

    clean up SignalFlags

--- Core/GObject/SignalFlags.cs
+++ Core/GObject/SignalFlags.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using GISharp.Runtime;
 
 namespace GISharp.GObject
 {
     /// <summary>
-    /// The signal flags are used to specify a signal's behaviour, the overall
+    /// The signal flags are used to specify a signal's behavior, the overall
     /// signal description outlines how especially the RUN flags control the
     /// stages of a signal emission.
     /// </summary>
@@ -13,30 +14,30 @@ namespace GISharp.GObject
         /// <summary>
         /// Invoke the object method handler in the first emission stage.
         /// </summary>
-        RunFirst = 1,
+        RunFirst = 0x00000001,
 
         /// <summary>
         /// Invoke the object method handler in the third emission stage.
         /// </summary>
-        RunLast = 2,
+        RunLast = 0x00000002,
 
         /// <summary>
         /// Invoke the object method handler in the last emission stage.
         /// </summary>
-        RunCleanup = 4,
+        RunCleanup = 0x00000004,
 
         /// <summary>
         /// Signals being emitted for an object while currently being in
-        ///  emission for this very object will not be emitted recursively,
-        ///  but instead cause the first emission to be restarted.
+        /// emission for this very object will not be emitted recursively,
+        /// but instead cause the first emission to be restarted.
         /// </summary>
-        NoRecurse = 8,
+        NoRecurse = 0x00000008,
 
         /// <summary>
         /// This signal supports "::detail" appendices to the signal name
-        ///  upon handler connections and emissions.
+        /// upon handler connections and emissions.
         /// </summary>
-        Detailed = 16,
+        Detailed = 0x00000010,
 
         /// <summary>
         /// Action signals are signals that may freely be emitted on alive
@@ -46,24 +47,26 @@ namespace GISharp.GObject
         ///  of as object methods which can be called generically by
         ///  third-party code.
         /// </summary>
-        Action = 32,
+        Action = 0x00000020,
 
         /// <summary>
         /// No emissions hooks are supported for this signal.
         /// </summary>
-        NoHooks = 64,
+        NoHooks = 0x00000040,
 
         /// <summary>
         /// Varargs signal emission will always collect the
-        ///   arguments, even if there are no signal handlers connected.  Since 2.30.
+        /// arguments, even if there are no signal handlers connected.
         /// </summary>
-        MustCollect = 128,
+        [Since("2.30")]
+        MustCollect = 0x00000080,
 
         /// <summary>
         /// The signal is deprecated and will be removed
-        ///   in a future version. A warning will be generated if it is connected while
-        ///   running with G_ENABLE_DIAGNOSTIC=1.  Since 2.32.
+        /// in a future version. A warning will be generated if it is connected
+        /// while running with <c>G_ENABLE_DIAGNOSTIC=1</c>.
         /// </summary>
-        Deprecated = 256
+        [Since("2.32")]
+        Deprecated = 0x00000100,
     }
 }
