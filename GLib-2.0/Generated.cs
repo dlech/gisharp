1866e8862fd45f4337fe1716e3b60f213d422c0d
David Lechner <david@lechnology.com>
2018-02-04 00:23:06 -0600

    Generate Version class

--- GLib-2.0/Generated.cs
+++ GLib-2.0/Generated.cs
@@ -3808,4 +3808,142 @@ namespace GISharp.GLib
         /// </summary>
         KeepTranslations = 2
     }
+
+    public static partial class Version
+    {
+        /// <summary>
+        /// The major version number of the GLib library.
+        /// </summary>
+        /// <remarks>
+        /// Like #glib_major_version, but from the headers used at
+        /// application compile time, rather than from the library
+        /// linked against at application run time.
+        /// </remarks>
+        private const System.Int32 major = 2;
+
+        /// <summary>
+        /// The micro version number of the GLib library.
+        /// </summary>
+        /// <remarks>
+        /// Like #gtk_micro_version, but from the headers used at
+        /// application compile time, rather than from the library
+        /// linked against at application run time.
+        /// </remarks>
+        private const System.Int32 micro = 3;
+
+        /// <summary>
+        /// The minor version number of the GLib library.
+        /// </summary>
+        /// <remarks>
+        /// Like #gtk_minor_version, but from the headers used at
+        /// application compile time, rather than from the library
+        /// linked against at application run time.
+        /// </remarks>
+        private const System.Int32 minor = 47;
+
+        /// <summary>
+        /// A macro that should be defined by the user prior to including
+        /// the glib.h header.
+        /// The definition should be one of the predefined GLib version
+        /// macros: %GLIB_VERSION_2_26, %GLIB_VERSION_2_28,...
+        /// </summary>
+        /// <remarks>
+        /// This macro defines the earliest version of GLib that the package is
+        /// required to be able to compile against.
+        /// 
+        /// If the compiler is configured to warn about the use of deprecated
+        /// functions, then using functions that were deprecated in version
+        /// %GLIB_VERSION_MIN_REQUIRED or earlier will cause warnings (but
+        /// using functions deprecated in later releases will not).
+        /// </remarks>
+        [GISharp.Runtime.SinceAttribute("2.32")]
+        private const System.Int32 minRequired = 2;
+
+        /// <summary>
+        /// Checks that the GLib library in use is compatible with the
+        /// given version. Generally you would pass in the constants
+        /// #GLIB_MAJOR_VERSION, #GLIB_MINOR_VERSION, #GLIB_MICRO_VERSION
+        /// as the three arguments to this function; that produces
+        /// a check that the library in use is compatible with
+        /// the version of GLib the application or module was compiled
+        /// against.
+        /// </summary>
+        /// <remarks>
+        /// Compatibility is defined by two things: first the version
+        /// of the running library is newer than the version
+        /// @required_major.required_minor.@required_micro. Second
+        /// the running library must be binary compatible with the
+        /// version @required_major.required_minor.@required_micro
+        /// (same major version.)
+        /// </remarks>
+        /// <param name="requiredMajor">
+        /// the required major version
+        /// </param>
+        /// <param name="requiredMinor">
+        /// the required minor version
+        /// </param>
+        /// <param name="requiredMicro">
+        /// the required micro version
+        /// </param>
+        /// <returns>
+        /// %NULL if the GLib library is compatible with the
+        ///     given version, or a string describing the version mismatch.
+        ///     The returned string is owned by GLib and must not be modified
+        ///     or freed.
+        /// </returns>
+        [GISharp.Runtime.SinceAttribute("2.6")]
+        [System.Runtime.InteropServices.DllImportAttribute("glib-2.0", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        /* <type name="utf8" type="const gchar*" managed-name="Utf8" /> */
+        /* transfer-ownership:none */
+        static extern System.IntPtr glib_check_version(
+        /* <type name="guint" type="guint" managed-name="Guint" /> */
+        /* transfer-ownership:none */
+        System.UInt32 requiredMajor,
+        /* <type name="guint" type="guint" managed-name="Guint" /> */
+        /* transfer-ownership:none */
+        System.UInt32 requiredMinor,
+        /* <type name="guint" type="guint" managed-name="Guint" /> */
+        /* transfer-ownership:none */
+        System.UInt32 requiredMicro);
+
+        /// <summary>
+        /// Checks that the GLib library in use is compatible with the
+        /// given version. Generally you would pass in the constants
+        /// #GLIB_MAJOR_VERSION, #GLIB_MINOR_VERSION, #GLIB_MICRO_VERSION
+        /// as the three arguments to this function; that produces
+        /// a check that the library in use is compatible with
+        /// the version of GLib the application or module was compiled
+        /// against.
+        /// </summary>
+        /// <remarks>
+        /// Compatibility is defined by two things: first the version
+        /// of the running library is newer than the version
+        /// @required_major.required_minor.@required_micro. Second
+        /// the running library must be binary compatible with the
+        /// version @required_major.required_minor.@required_micro
+        /// (same major version.)
+        /// </remarks>
+        /// <param name="requiredMajor">
+        /// the required major version
+        /// </param>
+        /// <param name="requiredMinor">
+        /// the required minor version
+        /// </param>
+        /// <param name="requiredMicro">
+        /// the required micro version
+        /// </param>
+        /// <returns>
+        /// %NULL if the GLib library is compatible with the
+        ///     given version, or a string describing the version mismatch.
+        ///     The returned string is owned by GLib and must not be modified
+        ///     or freed.
+        /// </returns>
+        [GISharp.Runtime.SinceAttribute("2.6")]
+        public static GISharp.GLib.Utf8 Check(System.UInt32 requiredMajor, System.UInt32 requiredMinor, System.UInt32 requiredMicro)
+        {
+            var ret_ = glib_check_version(requiredMajor, requiredMinor, requiredMicro);
+            var ret = GISharp.Runtime.Opaque.GetInstance<GISharp.GLib.Utf8>(ret_, GISharp.Runtime.Transfer.None);
+            return ret;
+        }
+    }
 }
