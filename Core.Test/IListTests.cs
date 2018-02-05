01dde25a9659b39a634575c6bb5f585e138eed0a
David Lechner <david@lechnology.com>
2018-02-04 20:05:46 -0600

    Add using blocks to IListTests

--- Core.Test/IListTests.cs
+++ Core.Test/IListTests.cs
@@ -25,265 +25,265 @@ namespace GISharp.Core.Test
         [Test]
         public void TestCount ()
         {
-            var l = new TList ();
-            Assert.That (l.Count, Is.EqualTo (0));
+            using (var l = new TList()) {
+                Assert.That(l.Count, Is.EqualTo(0));
 
-            l.Dispose ();
-            Assert.Throws<ObjectDisposedException> (() => l.Count.GetType ());
-
+                l.Dispose();
+                Assert.That(() => l.Count, Throws.TypeOf<ObjectDisposedException>());
+            }
             Utility.AssertNoGLibLog();
         }
 
         [Test]
         public void TestIsReadOnly ()
         {
-            var l = new TList ();
-            Assert.That (l.IsReadOnly, Is.False);
-
+            using (var l = new TList()) {
+                Assert.That(l.IsReadOnly, Is.False);
+            }
             Utility.AssertNoGLibLog();
         }
 
         [Test]
         public void TestIndexer ()
         {
-            var l = new TList ();
-            Assume.That (l.Count, Is.EqualTo (0));
+            using (var l = new TList()) {
+                Assume.That(l.Count, Is.EqualTo(0));
 
-            Assert.That (() => l[-1], Throws.TypeOf<ArgumentOutOfRangeException> ());
-            Assert.That (() => l[0], Throws.TypeOf<ArgumentOutOfRangeException> ());
+                Assert.That(() => l[-1], Throws.TypeOf<ArgumentOutOfRangeException>());
+                Assert.That(() => l[0], Throws.TypeOf<ArgumentOutOfRangeException>());
 
-            l.Add(values[1]);
-            Assert.That(l[0], Is.EqualTo(values[1]));
+                l.Add(values[1]);
+                Assert.That(l[0], Is.EqualTo(values[1]));
 
-            l[0] = values[2];
-            Assert.That(l[0], Is.EqualTo(values[2]));
+                l[0] = values[2];
+                Assert.That(l[0], Is.EqualTo(values[2]));
 
-            l.Dispose ();
-            Assert.That (() => l[0], Throws.TypeOf<ObjectDisposedException> ());
-
+                l.Dispose();
+                Assert.That(() => l[0], Throws.TypeOf<ObjectDisposedException>());
+            }
             Utility.AssertNoGLibLog();
         }
 
         [Test]
         public void TestAdd ()
         {
-            var l = new TList ();
-            Assume.That (l.Count, Is.EqualTo (0));
+            using (var l = new TList ()) {
+                Assume.That(l.Count, Is.EqualTo(0));
 
-            l.Add(values[1]);
-            Assert.That (l.Count, Is.EqualTo (1));
-            Assert.That(getItemAt(l, 0), Is.EqualTo(values[1]));
+                l.Add(values[1]);
+                Assert.That(l.Count, Is.EqualTo(1));
+                Assert.That(getItemAt(l, 0), Is.EqualTo(values[1]));
 
-            l.Dispose ();
-            Assert.That (() => l.Count, Throws.TypeOf<ObjectDisposedException> ());
-
+                l.Dispose();
+                Assert.That(() => l.Count, Throws.TypeOf<ObjectDisposedException>());
+            }
             Utility.AssertNoGLibLog();
         }
 
         [Test]
         public void TestClear ()
         {
-            var l = new TList ();
-            l.Add(values[1]);
-            Assume.That (l.Count, Is.EqualTo (1));
+            using (var l = new TList ()) {
+                l.Add(values[1]);
+                Assume.That(l.Count, Is.EqualTo(1));
 
-            l.Clear ();
-            Assert.That (l.Count, Is.EqualTo (0));
+                l.Clear();
+                Assert.That(l.Count, Is.EqualTo(0));
 
-            l.Dispose ();
-            Assert.That (() => l.Clear (), Throws.TypeOf<ObjectDisposedException> ());
-
+                l.Dispose();
+                Assert.That(() => l.Clear(), Throws.TypeOf<ObjectDisposedException>());
+            }
             Utility.AssertNoGLibLog();
         }
 
         [Test]
         public void TestContains ()
         {
-            var l = new TList ();
-            l.Add(values[1]);
+            using (var l = new TList ()) {
+                l.Add(values[1]);
 
-            Assert.That(l.Contains(values[0]), Is.False);
-            Assert.That(l.Contains(values[1]), Is.True);
+                Assert.That(l.Contains(values[0]), Is.False);
+                Assert.That(l.Contains(values[1]), Is.True);
 
-            l.Dispose ();
-            Assert.That(() => l.Contains(values[0]), Throws.TypeOf<ObjectDisposedException>());
-
+                l.Dispose();
+                Assert.That(() => l.Contains(values[0]), Throws.TypeOf<ObjectDisposedException>());
+            }
             Utility.AssertNoGLibLog();
         }
 
         [Test]
         public void TestCopyTo ()
         {
-            var l = new TList ();
-            l.Add(values[1]);
-            l.Add(values[2]);
-            Assume.That (l.Count, Is.EqualTo (2));
+            using (var l = new TList()) {
+                l.Add(values[1]);
+                l.Add(values[2]);
+                Assume.That(l.Count, Is.EqualTo(2));
 
-            var a = new TItem[3];
-            // TItem may not be a value type, so we need to explicitly initialize
-            // the array with values.
-            for (int i = 0; i < 3; i++) {
-                a[i] = values[0];
-                Assume.That(a[i], Is.EqualTo(values[0]));
+                var a = new TItem[3];
+                // TItem may not be a value type, so we need to explicitly initialize
+                // the array with values.
+                for (int i = 0; i < 3; i++) {
+                    a[i] = values[0];
+                    Assume.That(a[i], Is.EqualTo(values[0]));
+                }
+                l.CopyTo(a, 0);
+                Assert.That(a[0], Is.EqualTo(values[1]));
+                Assert.That(a[1], Is.EqualTo(values[2]));
+                Assert.That(a[2], Is.EqualTo(values[0]));
+
+                l.CopyTo (a, 1);
+                Assert.That(a[0], Is.EqualTo(values[1]));
+                Assert.That(a[1], Is.EqualTo(values[1]));
+                Assert.That(a[2], Is.EqualTo(values[2]));
+
+                Assert.That(() => a.CopyTo(null, 0), Throws.TypeOf<ArgumentNullException>());
+                Assert.That(() => a.CopyTo(a, -1), Throws.TypeOf<ArgumentOutOfRangeException>());
+                Assert.That(() => a.CopyTo(a, 2), Throws.TypeOf<ArgumentException>());
+
+                l.Dispose();
+                Assert.That(() => l.CopyTo(a, 0), Throws.TypeOf<ObjectDisposedException>());
             }
-            l.CopyTo(a, 0);
-            Assert.That(a[0], Is.EqualTo(values[1]));
-            Assert.That(a[1], Is.EqualTo(values[2]));
-            Assert.That(a[2], Is.EqualTo(values[0]));
-
-            l.CopyTo (a, 1);
-            Assert.That(a[0], Is.EqualTo(values[1]));
-            Assert.That(a[1], Is.EqualTo(values[1]));
-            Assert.That(a[2], Is.EqualTo(values[2]));
-
-            Assert.That (() => a.CopyTo (null, 0), Throws.TypeOf<ArgumentNullException> ());
-            Assert.That (() => a.CopyTo (a, -1), Throws.TypeOf<ArgumentOutOfRangeException> ());
-            Assert.That (() => a.CopyTo (a, 2), Throws.TypeOf<ArgumentException> ());
-
-            l.Dispose ();
-            Assert.That (() => l.CopyTo (a, 0), Throws.TypeOf<ObjectDisposedException> ());
-
             Utility.AssertNoGLibLog();
         }
 
         [Test]
         public void TestGetEnumerator ()
         {
-            var l = new TList ();
-            l.Add(values[1]);
-            l.Add(values[2]);
-            l.Add(values[3]);
+            using (var l = new TList()) {
+                l.Add(values[1]);
+                l.Add(values[2]);
+                l.Add(values[3]);
 
-            var expected = 1;
-            foreach (var i in l) {
-                Assert.That (i, Is.EqualTo (values[expected++]));
+                var expected = 1;
+                foreach (var i in l) {
+                    Assert.That(i, Is.EqualTo(values[expected++]));
+                }
+                Assert.That(expected, Is.EqualTo(4));
+
+                l.Dispose();
+                Assert.That(() => l.Any(), Throws.TypeOf<ObjectDisposedException>());
             }
-            Assert.That (expected, Is.EqualTo (4));
-
-            l.Dispose ();
-            Assert.That(() => l.Any(), Throws.TypeOf<ObjectDisposedException>());
-
             Utility.AssertNoGLibLog();
         }
 
         [Test]
         public void TestIndexOf ()
         {
-            var l = new TList ();
-            l.Add(values[1]);
-            l.Add(values[1]);
+            using (var l = new TList()) {
+                l.Add(values[1]);
+                l.Add(values[1]);
 
-            Assert.That(l.IndexOf(values[0]), Is.EqualTo(-1));
-            Assert.That(l.IndexOf(values[1]), Is.EqualTo(0));
+                Assert.That(l.IndexOf(values[0]), Is.EqualTo(-1));
+                Assert.That(l.IndexOf(values[1]), Is.EqualTo(0));
 
-            l.Dispose ();
-            Assert.That(() => l.IndexOf(values[0]), Throws.TypeOf<ObjectDisposedException> ());
-
+                l.Dispose();
+                Assert.That(() => l.IndexOf(values[0]), Throws.TypeOf<ObjectDisposedException>());
+            }
             Utility.AssertNoGLibLog();
         }
 
         [Test]
         public void TestInsert ()
         {
-            var l = new TList ();
-            Assume.That (l.Count, Is.EqualTo (0));
+            using (var l = new TList()) {
+                Assume.That(l.Count, Is.EqualTo(0));
 
-            l.Insert(0, values[1]);
-            Assert.That (l.Count, Is.EqualTo (1));
-            Assert.That(getItemAt(l, 0), Is.EqualTo(values[1]));
+                l.Insert(0, values[1]);
+                Assert.That(l.Count, Is.EqualTo(1));
+                Assert.That(getItemAt(l, 0), Is.EqualTo(values[1]));
 
-            l.Insert(0, values[2]);
-            Assert.That (l.Count, Is.EqualTo (2));
-            Assert.That(getItemAt(l, 0), Is.EqualTo(values[2]));
-            Assert.That(getItemAt(l, 1), Is.EqualTo(values[1]));
+                l.Insert(0, values[2]);
+                Assert.That(l.Count, Is.EqualTo(2));
+                Assert.That(getItemAt(l, 0), Is.EqualTo(values[2]));
+                Assert.That(getItemAt(l, 1), Is.EqualTo(values[1]));
 
-            l.Insert(1, values[3]);
-            Assert.That (l.Count, Is.EqualTo (3));
-            Assert.That(getItemAt(l, 0), Is.EqualTo(values[2]));
-            Assert.That(getItemAt(l, 1), Is.EqualTo(values[3]));
-            Assert.That(getItemAt(l, 2), Is.EqualTo(values[1]));
+                l.Insert(1, values[3]);
+                Assert.That(l.Count, Is.EqualTo(3));
+                Assert.That(getItemAt(l, 0), Is.EqualTo(values[2]));
+                Assert.That(getItemAt(l, 1), Is.EqualTo(values[3]));
+                Assert.That(getItemAt(l, 2), Is.EqualTo(values[1]));
 
-            l.Insert(3, values[4]);
-            Assert.That (l.Count, Is.EqualTo (4));
-            Assert.That(getItemAt(l, 0), Is.EqualTo(values[2]));
-            Assert.That(getItemAt(l, 1), Is.EqualTo(values[3]));
-            Assert.That(getItemAt(l, 2), Is.EqualTo(values[1]));
-            Assert.That(getItemAt(l, 3), Is.EqualTo(values[4]));
+                l.Insert(3, values[4]);
+                Assert.That(l.Count, Is.EqualTo(4));
+                Assert.That(getItemAt(l, 0), Is.EqualTo(values[2]));
+                Assert.That(getItemAt(l, 1), Is.EqualTo(values[3]));
+                Assert.That(getItemAt(l, 2), Is.EqualTo(values[1]));
+                Assert.That(getItemAt(l, 3), Is.EqualTo(values[4]));
 
-            Assert.That(() => l.Insert(-1, values[0]), Throws.TypeOf<ArgumentOutOfRangeException>());
-            Assert.That(() => l.Insert(5, values[0]), Throws.TypeOf<ArgumentOutOfRangeException>());
+                Assert.That(() => l.Insert(-1, values[0]), Throws.TypeOf<ArgumentOutOfRangeException>());
+                Assert.That(() => l.Insert(5, values[0]), Throws.TypeOf<ArgumentOutOfRangeException>());
 
-            l.Dispose ();
-            Assert.That(() => l.Insert(0, values[0]), Throws.TypeOf<ObjectDisposedException>());
-
+                l.Dispose();
+                Assert.That(() => l.Insert(0, values[0]), Throws.TypeOf<ObjectDisposedException>());
+            }
             Utility.AssertNoGLibLog();
         }
 
         [Test]
         public void TestRemove ()
         {
-            var l = new TList ();
-            Assume.That (l.Count, Is.EqualTo (0));
+            using (var l = new TList()) {
+                Assume.That(l.Count, Is.EqualTo(0));
 
-            Assert.That(l.Remove(values[0]), Is.False);
-            Assert.That (l.Count, Is.EqualTo (0));
+                Assert.That(l.Remove(values[0]), Is.False);
+                Assert.That(l.Count, Is.EqualTo(0));
 
-            l.Add(values[1]);
-            Assume.That (l.Count, Is.EqualTo (1));
+                l.Add(values[1]);
+                Assume.That(l.Count, Is.EqualTo(1));
 
-            Assert.That(l.Remove(values[0]), Is.False);
-            Assert.That (l.Count, Is.EqualTo (1));
-            Assert.That(l.Remove(values[1]), Is.True);
-            Assert.That (l.Count, Is.EqualTo (0));
+                Assert.That(l.Remove(values[0]), Is.False);
+                Assert.That(l.Count, Is.EqualTo(1));
+                Assert.That(l.Remove(values[1]), Is.True);
+                Assert.That(l.Count, Is.EqualTo(0));
 
-            l.Add(values[1]);
-            l.Add(values[2]);
-            l.Add(values[3]);
-            l.Add(values[4]);
-            Assume.That (l.Count, Is.EqualTo (4));
+                l.Add(values[1]);
+                l.Add(values[2]);
+                l.Add(values[3]);
+                l.Add(values[4]);
+                Assume.That(l.Count, Is.EqualTo(4));
 
-            Assert.That(l.Remove(values[0]), Is.False);
-            Assert.That (l.Count, Is.EqualTo (4));
-            Assert.That(l.Remove(values[2]), Is.True);
-            Assert.That (l.Count, Is.EqualTo (3));
-            Assert.That(getItemAt(l, 0), Is.EqualTo(values[1]));
-            Assert.That(getItemAt(l, 1), Is.EqualTo(values[3]));
-            Assert.That(getItemAt(l, 2), Is.EqualTo(values[4]));
+                Assert.That(l.Remove(values[0]), Is.False);
+                Assert.That(l.Count, Is.EqualTo(4));
+                Assert.That(l.Remove(values[2]), Is.True);
+                Assert.That(l.Count, Is.EqualTo(3));
+                Assert.That(getItemAt(l, 0), Is.EqualTo(values[1]));
+                Assert.That(getItemAt(l, 1), Is.EqualTo(values[3]));
+                Assert.That(getItemAt(l, 2), Is.EqualTo(values[4]));
 
-            l.Dispose ();
-            Assert.That(() => l.Remove(values[0]), Throws.TypeOf<ObjectDisposedException>());
-
+                l.Dispose();
+                Assert.That(() => l.Remove(values[0]), Throws.TypeOf<ObjectDisposedException>());
+            }
             Utility.AssertNoGLibLog();
         }
 
         [Test]
         public void TestRemoveAt ()
         {
-            var l = new TList ();
-            l.Add(values[1]);
-            Assume.That (l.Count, Is.EqualTo (1));
+            using (var l = new TList()) {
+                l.Add(values[1]);
+                Assume.That(l.Count, Is.EqualTo(1));
 
-            l.RemoveAt (0);
-            Assert.That (l.Count, Is.EqualTo (0));
+                l.RemoveAt(0);
+                Assert.That(l.Count, Is.EqualTo(0));
 
-            l.Add(values[1]);
-            l.Add(values[2]);
-            l.Add(values[3]);
-            l.Add(values[4]);
-            Assume.That (l.Count, Is.EqualTo (4));
+                l.Add(values[1]);
+                l.Add(values[2]);
+                l.Add(values[3]);
+                l.Add(values[4]);
+                Assume.That(l.Count, Is.EqualTo(4));
 
-            l.RemoveAt (1);
-            Assert.That (l.Count, Is.EqualTo (3));
-            Assert.That(getItemAt(l, 0), Is.EqualTo(values[1]));
-            Assert.That(getItemAt(l, 1), Is.EqualTo(values[3]));
-            Assert.That(getItemAt(l, 2), Is.EqualTo(values[4]));
+                l.RemoveAt(1);
+                Assert.That(l.Count, Is.EqualTo(3));
+                Assert.That(getItemAt(l, 0), Is.EqualTo(values[1]));
+                Assert.That(getItemAt(l, 1), Is.EqualTo(values[3]));
+                Assert.That(getItemAt(l, 2), Is.EqualTo(values[4]));
 
-            Assert.That (() => l.RemoveAt (-1), Throws.TypeOf<ArgumentOutOfRangeException> ());
-            Assert.That (() => l.RemoveAt (3), Throws.TypeOf<ArgumentOutOfRangeException> ());
+                Assert.That(() => l.RemoveAt(-1), Throws.TypeOf<ArgumentOutOfRangeException>());
+                Assert.That(() => l.RemoveAt(3), Throws.TypeOf<ArgumentOutOfRangeException>());
 
-            l.Dispose ();
-            Assert.That (() => l.RemoveAt (0), Throws.TypeOf<ObjectDisposedException> ());
-
+                l.Dispose();
+                Assert.That(() => l.RemoveAt(0), Throws.TypeOf<ObjectDisposedException>());
+            }
             Utility.AssertNoGLibLog();
         }
     }
