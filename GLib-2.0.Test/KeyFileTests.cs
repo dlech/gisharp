6aa08f9a711409c34924785ff6d20c9c6591760c
David Lechner <david@lechnology.com>
2018-02-08 16:46:45 -0600

    Write some tests for GLib.KeyFile

---
+++ GLib-2.0.Test/KeyFileTests.cs
@@ -0,0 +1,224 @@
+
+using NUnit.Framework;
+
+using GISharp.GLib;
+using GISharp.Runtime;
+
+using static GISharp.TestHelpers;
+
+namespace GISharp.GLib.Test
+{
+    [TestFixture]
+    [TestOf(typeof(KeyFile))]
+    public class KeyFileTests
+    {
+        static readonly Utf8 TestGroup = "TestGroup";
+        static readonly Utf8 TestKey1 = "TestKey1";
+        static readonly Utf8 TestKey2 = "TestKey2";
+
+        [Test]
+        public void TestString()
+        {
+            using (var keyFile = new KeyFile()) {
+                Assert.That(() => keyFile.GetString(TestGroup, TestKey1),
+                    ThrowsGErrorException(KeyFileError.GroupNotFound),
+                    "Trying to get a non-existant group should throw an exception");
+
+                using (var expected = (Utf8)("Test String")) {
+                    keyFile.SetString(TestGroup, TestKey1, expected);
+                    var actual = keyFile.GetString(TestGroup, TestKey1);
+                    Assert.That(actual, IsEqualToUtf8(expected));
+                }
+
+                Assert.That(() => keyFile.GetString(TestGroup, TestKey2),
+                    ThrowsGErrorException(KeyFileError.KeyNotFound),
+                    "Trying to get a non-existant key should throw an exception");
+            }
+            AssertNoGLibLog();
+        }
+
+        [Test]
+        public void TestBoolean()
+        {
+            using (var keyFile = new KeyFile()) {
+                Assert.That(() => keyFile.GetBoolean(TestGroup, TestKey1),
+                    ThrowsGErrorException(KeyFileError.GroupNotFound),
+                    "Trying to get a non-existant group should throw an exception");
+
+                const bool expected = true;
+                keyFile.SetBoolean(TestGroup, TestKey1, expected);
+                var actual = keyFile.GetBoolean(TestGroup, TestKey1);
+                Assert.That(actual, Is.EqualTo(expected));
+
+                Assert.That(() => keyFile.GetBoolean(TestGroup, TestKey2),
+                    ThrowsGErrorException(KeyFileError.KeyNotFound),
+                    "Trying to get a non-existant key should throw an exception");
+            }
+            AssertNoGLibLog();
+        }
+
+        [Test]
+        public void TestInteger()
+        {
+            using (var keyFile = new KeyFile()) {
+                Assert.That(() => keyFile.GetInteger(TestGroup, TestKey1),
+                    ThrowsGErrorException(KeyFileError.GroupNotFound),
+                    "Trying to get a non-existant group should throw an exception");
+
+                const int expected = 1;
+                keyFile.SetInteger(TestGroup, TestKey1, expected);
+                var actual = keyFile.GetInteger(TestGroup, TestKey1);
+                Assert.That(actual, Is.EqualTo(expected));
+
+                Assert.That(() => keyFile.GetInteger(TestGroup, TestKey2),
+                    ThrowsGErrorException(KeyFileError.KeyNotFound),
+                    "Trying to get a non-existant key should throw an exception");
+            }
+            AssertNoGLibLog();
+        }
+
+        [Test]
+        public void TestInt64()
+        {
+            using (var keyFile = new KeyFile()) {
+                Assert.That(() => keyFile.GetInt64(TestGroup, TestKey1),
+                    ThrowsGErrorException(KeyFileError.GroupNotFound),
+                    "Trying to get a non-existant group should throw an exception");
+
+                const long expected = 1;
+                keyFile.SetInt64(TestGroup, TestKey1, expected);
+                var actual = keyFile.GetInt64(TestGroup, TestKey1);
+                Assert.That(actual, Is.EqualTo(expected));
+
+                Assert.That(() => keyFile.GetInt64(TestGroup, TestKey2),
+                    ThrowsGErrorException(KeyFileError.KeyNotFound),
+                    "Trying to get a non-existant key should throw an exception");
+            }
+            AssertNoGLibLog();
+        }
+
+        [Test]
+        public void TestUInt64()
+        {
+            using (var keyFile = new KeyFile()) {
+                Assert.That(() => keyFile.GetUint64(TestGroup, TestKey1),
+                    ThrowsGErrorException(KeyFileError.GroupNotFound),
+                    "Trying to get a non-existant group should throw an exception");
+
+                const ulong expected = 1;
+                keyFile.SetUint64(TestGroup, TestKey1, expected);
+                var actual = keyFile.GetUint64(TestGroup, TestKey1);
+                Assert.That(actual, Is.EqualTo(expected));
+
+                Assert.That(() => keyFile.GetUint64(TestGroup, TestKey2),
+                    ThrowsGErrorException(KeyFileError.KeyNotFound),
+                    "Trying to get a non-existant key should throw an exception");
+            }
+            AssertNoGLibLog();
+        }
+
+        [Test]
+        public void TestDouble()
+        {
+            using (var keyFile = new KeyFile()) {
+                Assert.That(() => keyFile.GetDouble(TestGroup, TestKey1),
+                    ThrowsGErrorException(KeyFileError.GroupNotFound),
+                    "Trying to get a non-existant group should throw an exception");
+
+                const double expected = 1;
+                keyFile.SetDouble(TestGroup, TestKey1, expected);
+                var actual = keyFile.GetDouble(TestGroup, TestKey1);
+                Assert.That(actual, Is.EqualTo(expected));
+
+                Assert.That(() => keyFile.GetDouble(TestGroup, TestKey2),
+                    ThrowsGErrorException(KeyFileError.KeyNotFound),
+                    "Trying to get a non-existant key should throw an exception");
+            }
+            AssertNoGLibLog();
+        }
+
+        [Test]
+        public void TestStringList()
+        {
+            using (var keyFile = new KeyFile()) {
+                Assert.That(() => keyFile.GetStringList(TestGroup, TestKey1),
+                    ThrowsGErrorException(KeyFileError.GroupNotFound),
+                    "Trying to get a non-existant group should throw an exception");
+
+                using (var expected = new Strv("Item 1", "Item 2")) {
+                    keyFile.SetStringList(TestGroup, TestKey1, expected);
+                    var actual = keyFile.GetStringList(TestGroup, TestKey1);
+                    Assert.That(actual, Is.EqualTo(expected));
+                }
+
+                Assert.That(() => keyFile.GetStringList(TestGroup, TestKey2),
+                    ThrowsGErrorException(KeyFileError.KeyNotFound),
+                    "Trying to get a non-existant key should throw an exception");
+            }
+            AssertNoGLibLog();
+        }
+
+        [Test]
+        public void TestBooleanList()
+        {
+            using (var keyFile = new KeyFile()) {
+                Assert.That(() => keyFile.GetBooleanList(TestGroup, TestKey1),
+                    ThrowsGErrorException(KeyFileError.GroupNotFound),
+                    "Trying to get a non-existant group should throw an exception");
+
+                using (var expected = new Array<bool>{ true, false }) {
+                    keyFile.SetBooleanList(TestGroup, TestKey1, expected);
+                    var actual = keyFile.GetBooleanList(TestGroup, TestKey1);
+                    Assert.That(actual, Is.EqualTo(expected));
+                }
+
+                Assert.That(() => keyFile.GetBooleanList(TestGroup, TestKey2),
+                    ThrowsGErrorException(KeyFileError.KeyNotFound),
+                    "Trying to get a non-existant key should throw an exception");
+            }
+            AssertNoGLibLog();
+        }
+
+        [Test]
+        public void TestIntegerList()
+        {
+            using (var keyFile = new KeyFile()) {
+                Assert.That(() => keyFile.GetIntegerList(TestGroup, TestKey1),
+                    ThrowsGErrorException(KeyFileError.GroupNotFound),
+                    "Trying to get a non-existant group should throw an exception");
+
+                using (var expected = new Array<int>{ 1, 2 }) {
+                    keyFile.SetIntegerList(TestGroup, TestKey1, expected);
+                    var actual = keyFile.GetIntegerList(TestGroup, TestKey1);
+                    Assert.That(actual, Is.EqualTo(expected));
+                }
+
+                Assert.That(() => keyFile.GetIntegerList(TestGroup, TestKey2),
+                    ThrowsGErrorException(KeyFileError.KeyNotFound),
+                    "Trying to get a non-existant key should throw an exception");
+            }
+            AssertNoGLibLog();
+        }
+
+        [Test]
+        public void TestDoubleList()
+        {
+            using (var keyFile = new KeyFile()) {
+                Assert.That(() => keyFile.GetDoubleList(TestGroup, TestKey1),
+                    ThrowsGErrorException(KeyFileError.GroupNotFound),
+                    "Trying to get a non-existant group should throw an exception");
+
+                using (var expected = new Array<double>{ 1, 2 }) {
+                    keyFile.SetDoubleList(TestGroup, TestKey1, expected);
+                    var actual = keyFile.GetDoubleList(TestGroup, TestKey1);
+                    Assert.That(actual, Is.EqualTo(expected));
+                }
+
+                Assert.That(() => keyFile.GetDoubleList(TestGroup, TestKey2),
+                    ThrowsGErrorException(KeyFileError.KeyNotFound),
+                    "Trying to get a non-existant key should throw an exception");
+            }
+            AssertNoGLibLog();
+        }
+    }
+}
--- TestHelpers/TestHelpers.cs
+++ TestHelpers/TestHelpers.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using GISharp.GLib;
 using GISharp.GModule;
+using GISharp.Runtime;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace GISharp
 {
@@ -49,6 +53,49 @@ namespace GISharp
             if (properties.ContainsKey(warning)) {
                 Assert.Fail("{0}", properties.Get(warning));
             }
+        }
+
+        sealed class Utf8EqualConstraint : EqualConstraint
+        {
+            public Utf8EqualConstraint(Utf8 expected) : base((string)expected)
+            {
+            }
+
+            public override ConstraintResult ApplyTo<TActual>(TActual actual)
+            {
+                if (actual is Utf8 utf8) {
+                    return base.ApplyTo<string>(utf8);
+                }
+                return base.ApplyTo<TActual>(actual);
+            }
+        }
+
+        /// <summary>
+        /// Use this instead of <see cref="NUnit.Framework.Is.EqualTo(object)"/>
+        /// when comparing <see cref="GISharp.GLib.Utf8"/> strings in order to
+        /// get a useful failure message.
+        /// </summary>
+        public static Constraint IsEqualToUtf8(Utf8 utf8)
+        {
+            return new Utf8EqualConstraint(utf8);
+        }
+
+        /// <summary>
+        /// Use this instead of <see cref="NUnit.Framework.Throws.TypeOf{GISharp.Runtime.GErrorException}"/>
+        /// to save a bunch of typing.
+        /// </summary>
+        /// <param name="code">
+        /// The expected error code. The error domain will be inferred from the
+        /// see <see cref="GISharp.Runtime.GErrorDomainAttribute"/> of the
+        /// enum type.
+        /// </param>
+        public static Constraint ThrowsGErrorException(Enum code)
+        {
+            var domain = code.GetGErrorDomain();
+            var value = Convert.ToInt32(code);
+            return Throws.TypeOf<GErrorException>()
+                .With.Property("Error").Property("Domain").EqualTo(domain)
+                .And.Property("Error").Property("Code").EqualTo(value);
         }
     }
 }
