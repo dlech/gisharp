146c695d4f01d0c24170b111a36ab4b48f208175
David Lechner <david@lechnology.com>
2018-02-25 13:20:16 -0600

    GLib: implement OptionFlags

--- GLib-2.0/Generated.cs
+++ GLib-2.0/Generated.cs
@@ -3767,6 +3767,60 @@ namespace GISharp.GLib
     }
 
     /// <summary>
+    /// Flags which modify individual options.
+    /// </summary>
+    [System.FlagsAttribute]
+    public enum OptionFlags
+    {
+        /// <summary>
+        /// No flags. Since: 2.42.
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// The option doesn't appear in `--help` output.
+        /// </summary>
+        Hidden = 1,
+        /// <summary>
+        /// The option appears in the main section of the
+        ///     `--help` output, even if it is defined in a group.
+        /// </summary>
+        InMain = 2,
+        /// <summary>
+        /// For options of the %G_OPTION_ARG_NONE kind, this
+        ///     flag indicates that the sense of the option is reversed.
+        /// </summary>
+        Reverse = 4,
+        /// <summary>
+        /// For options of the %G_OPTION_ARG_CALLBACK kind,
+        ///     this flag indicates that the callback does not take any argument
+        ///     (like a %G_OPTION_ARG_NONE option). Since 2.8
+        /// </summary>
+        NoArg = 8,
+        /// <summary>
+        /// For options of the %G_OPTION_ARG_CALLBACK
+        ///     kind, this flag indicates that the argument should be passed to the
+        ///     callback in the GLib filename encoding rather than UTF-8. Since 2.8
+        /// </summary>
+        Filename = 16,
+        /// <summary>
+        /// For options of the %G_OPTION_ARG_CALLBACK
+        ///     kind, this flag indicates that the argument supply is optional.
+        ///     If no argument is given then data of %GOptionParseFunc will be
+        ///     set to NULL. Since 2.8
+        /// </summary>
+        OptionalArg = 32,
+        /// <summary>
+        /// This flag turns off the automatic conflict
+        ///     resolution which prefixes long option names with `groupname-` if
+        ///     there is a conflict. This option should only be used in situations
+        ///     where aliasing is necessary to model some legacy commandline interface.
+        ///     It is not safe to use this option, unless all option groups are under
+        ///     your direct control. Since 2.8.
+        /// </summary>
+        NoAlias = 64
+    }
+
+    /// <summary>
     /// A utility type for constructing container-type #GVariant instances.
     /// </summary>
     /// <remarks>
