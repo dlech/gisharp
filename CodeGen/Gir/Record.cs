1105d005e1a6343d930a75b9ae665d697f9e2cbf
David Lechner <david@lechnology.com>
2018-03-24 16:52:09 -0500

    CodeGen: handle GType struct parent when there are no fields in a class's GType struct

--- CodeGen/Gir/Record.cs
+++ CodeGen/Gir/Record.cs
@@ -27,6 +27,12 @@ namespace GISharp.CodeGen.Gir
         public bool IsSource { get; }
 
         /// <summary>
+        /// Gets the type that this record is a gytpe struct for, if any
+        /// </summary>
+        public GIRegisteredType IsGTypeStructForType => _IsGTypeStructForType.Value;
+        readonly Lazy<GIRegisteredType> _IsGTypeStructForType;
+
+        /// <summary>
         /// Gets the base type for an opaque record. Not valid for records that
         /// are structs.
         /// </summary>
@@ -41,14 +47,25 @@ namespace GISharp.CodeGen.Gir
             IsDisguised = Element.Attribute("disguised").AsBool();
             IsGTypeStructFor = element.Attribute(glib + "is-gtype-struct-for").AsString();
             IsSource = element.Attribute(gs + "source").AsBool();
+            _IsGTypeStructForType = new Lazy<GIRegisteredType>(LazyGetIsGTypeStructForType, false);
             _BaseType = new Lazy<System.Type>(LazyGetBaseType, false);
         }
+
+        GIRegisteredType LazyGetIsGTypeStructForType() =>
+            (GIRegisteredType)Namespace.AllTypes.SingleOrDefault(x => x.GirName == IsGTypeStructFor);
 
         System.Type LazyGetBaseType()
         {
             if (IsGTypeStructFor != null) {
-                // in GType structs, the first field is always the base type
-                return Fields.First().Type.ManagedType;
+                if (Fields.Any()) {
+                    // in GType structs, the first field is always the base type
+                    return Fields.First().Type.ManagedType;
+                }
+                // if there weren't any fields, maybe it is a class
+                if (IsGTypeStructForType is Class @class) {
+                    return @class.ParentType.GetGTypeStruct();
+                }
+                throw new NotSupportedException("Don't know how to get the parent for this GType struct");
             }
             if (IsSource) {
                 return typeof(Source);
