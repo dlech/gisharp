9f032c69de7c109df08858eed0237d2d2538d584
David Lechner <david@lechnology.com>
2018-03-24 23:41:49 -0500

    Gio: implement ActionFlags

--- Gio-2.0/Generated.cs
+++ Gio-2.0/Generated.cs
@@ -3555,6 +3555,82 @@ System.IntPtr actionName);
     }
 
     /// <summary>
+    /// Flags used to define the behaviour of a #GApplication.
+    /// </summary>
+    [GISharp.Runtime.SinceAttribute("2.28")]
+    [GISharp.Runtime.GTypeAttribute("GApplicationFlags", IsProxyForUnmanagedType = true)]
+    [System.FlagsAttribute]
+    public enum ApplicationFlags
+    {
+        /// <summary>
+        /// Default
+        /// </summary>
+        FlagsNone = 0,
+        /// <summary>
+        /// Run as a service. In this mode, registration
+        ///      fails if the service is already running, and the application
+        ///      will initially wait up to 10 seconds for an initial activation
+        ///      message to arrive.
+        /// </summary>
+        IsService = 1,
+        /// <summary>
+        /// Don't try to become the primary instance.
+        /// </summary>
+        IsLauncher = 2,
+        /// <summary>
+        /// This application handles opening files (in
+        ///     the primary instance). Note that this flag only affects the default
+        ///     implementation of local_command_line(), and has no effect if
+        ///     %G_APPLICATION_HANDLES_COMMAND_LINE is given.
+        ///     See g_application_run() for details.
+        /// </summary>
+        HandlesOpen = 4,
+        /// <summary>
+        /// This application handles command line
+        ///     arguments (in the primary instance). Note that this flag only affect
+        ///     the default implementation of local_command_line().
+        ///     See g_application_run() for details.
+        /// </summary>
+        HandlesCommandLine = 8,
+        /// <summary>
+        /// Send the environment of the
+        ///     launching process to the primary instance. Set this flag if your
+        ///     application is expected to behave differently depending on certain
+        ///     environment variables. For instance, an editor might be expected
+        ///     to use the `GIT_COMMITTER_NAME` environment variable
+        ///     when editing a git commit message. The environment is available
+        ///     to the #GApplication::command-line signal handler, via
+        ///     g_application_command_line_getenv().
+        /// </summary>
+        SendEnvironment = 16,
+        /// <summary>
+        /// Make no attempts to do any of the typical
+        ///     single-instance application negotiation, even if the application
+        ///     ID is given.  The application neither attempts to become the
+        ///     owner of the application ID nor does it check if an existing
+        ///     owner already exists.  Everything occurs in the local process.
+        ///     Since: 2.30.
+        /// </summary>
+        NonUnique = 32,
+        /// <summary>
+        /// Allow users to override the
+        ///     application ID from the command line with `--gapplication-app-id`.
+        ///     Since: 2.48
+        /// </summary>
+        CanOverrideAppId = 64
+    }
+
+    public partial class ApplicationFlagsExtensions
+    {
+        static readonly GISharp.Lib.GObject.GType _GType = g_application_flags_get_type();
+
+        [System.Runtime.InteropServices.DllImportAttribute("gio-2.0", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        /* <type name="GType" type="GType" managed-name="GISharp.Lib.GObject.GType" /> */
+        /* transfer-ownership:full direction:out */
+        static extern GISharp.Lib.GObject.GType g_application_flags_get_type();
+    }
+
+    /// <summary>
     /// Type definition for a function that will be called back when an asynchronous
     /// operation within GIO has been completed.
     /// </summary>
