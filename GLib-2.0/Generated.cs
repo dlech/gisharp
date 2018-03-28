5004e7d6b031c2940cbd780c2b3b4ed04302ce2f
David Lechner <david@lechnology.com>
2018-03-27 21:48:09 -0500

    GLib: implement SeekType

--- GLib-2.0/Generated.cs
+++ GLib-2.0/Generated.cs
@@ -8328,6 +8328,26 @@ namespace GISharp.Lib.GLib
     }
 
     /// <summary>
+    /// An enumeration specifying the base position for a
+    /// g_io_channel_seek_position() operation.
+    /// </summary>
+    public enum SeekType
+    {
+        /// <summary>
+        /// the current position in the file.
+        /// </summary>
+        Current = 0,
+        /// <summary>
+        /// the start of the file.
+        /// </summary>
+        Start = 1,
+        /// <summary>
+        /// the end of the file.
+        /// </summary>
+        End = 2
+    }
+
+    /// <summary>
     /// Disambiguates a given time in two ways.
     /// </summary>
     /// <remarks>
