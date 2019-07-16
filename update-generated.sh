58307668177fba7592ad8820281794a2e1af2aa1
David Lechner <david@lechnology.com>
2019-07-16 18:22:09 -0500

    move GObject-2.0 to src/

--- update-generated.sh
+++ update-generated.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 dotnet run --project tools/CodeGen/CodeGen.csproj -- -p src/GLib-2.0 -c generate
-dotnet run --project tools/CodeGen/CodeGen.csproj -- -p GObject-2.0 -c generate
+dotnet run --project tools/CodeGen/CodeGen.csproj -- -p src/GObject-2.0 -c generate
 dotnet run --project tools/CodeGen/CodeGen.csproj -- -p Gio-2.0 -c generate
