c5ba65f7a5f6af6e446ba8df519a71b24f839002
David Lechner <david@lechnology.com>
2019-07-16 18:38:02 -0500

    Move Gio-2.0 to src/

--- update-generated.sh
+++ update-generated.sh
@@ -2,4 +2,4 @@
 
 dotnet run --project tools/CodeGen/CodeGen.csproj -- -p src/GLib-2.0 -c generate
 dotnet run --project tools/CodeGen/CodeGen.csproj -- -p src/GObject-2.0 -c generate
-dotnet run --project tools/CodeGen/CodeGen.csproj -- -p Gio-2.0 -c generate
+dotnet run --project tools/CodeGen/CodeGen.csproj -- -p src/Gio-2.0 -c generate
