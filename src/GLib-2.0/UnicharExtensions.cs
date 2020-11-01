5fef34af63bf698d331e949b3bfab7fabb22e6d6
David Lechner <david@lechnology.com>
2020-10-31 23:59:20 -0500

    make Unichar methods extension methods
    this reduces the amount of code needed to be hand-maintained in Core

---
+++ src/GLib-2.0/UnicharExtensions.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Runtime.InteropServices;
+using GISharp.Runtime;
+
+namespace GISharp.Lib.GLib
+{
+    partial class UnicharExtensions
+    {
+        /// <summary>
+        /// Converts a single character to UTF-8.
+        /// </summary>
+        /// <param name="ch">
+        /// a Unicode character.
+        /// </param>
+        public static Utf8 ToUtf8(this Unichar ch)
+        {
+            var outbuf_ = GMarshal.Alloc0(6);
+            g_unichar_to_utf8(ch, outbuf_);
+            var outbuf = new Utf8(outbuf_, Transfer.Full);
+            return outbuf;
+        }
+
+        /// <include file="UnicharExtensions.xmldoc" path="declaration/member[@name='UnicharExtensions.FullyDecompose(GISharp.Lib.GLib.Unichar,System.Boolean)']/*" />
+        [Since("2.30")]
+        public static Memory<Unichar> FullyDecompose(this Unichar ch, bool compat = false)
+        {
+            Memory<Unichar> result = new Unichar[maxDecompositionLength];
+            ref readonly var result_ = ref MemoryMarshal.GetReference(result.Span);
+            var resultLen_ = (UIntPtr)maxDecompositionLength;
+            var ret = g_unichar_fully_decompose(ch, compat, result_, resultLen_);
+            return result.Slice(0, (int)ret);
+        }
+    }
+}
