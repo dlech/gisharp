dfe208dab648fd42d20af3004ff74e52418a1211
David Lechner <david@lechnology.com>
2021-02-15 22:00:30 -0600

    GIRepository-2.0: add Version class

---
+++ src/GIRepository-2.0/Version.cs
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2021 David Lechner <david@lechnology.com>
+
+namespace GISharp.Lib.GIRepository
+{
+    partial class Version
+    {
+        /// <summary>
+        /// The version of the library that was used at compile time.
+        /// </summary>
+        /// <value>The compile time version.</value>
+        public static System.Version CompileTime => new(
+            compileTimeMajorVersion,
+            compileTimeMinorVersion,
+            compileTimeMicroVersion,
+            0);
+
+        /// <summary>
+        /// The version of the library linked against at run time.
+        /// </summary>
+        /// <value>The run time.</value>
+        public static System.Version RunTime => new(
+            (int)RunTimeMajorVersion,
+            (int)RunTimeMinorVersion,
+            (int)RunTimeMicroVersion,
+            0);
+    }
+}
