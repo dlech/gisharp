24c9efeaf3ccdb5793cc7d993ac1f9886af1a8b0
David Lechner <david@lechnology.com>
2021-03-03 23:01:37 -0600

    fix coverage
    coverlet does not allow '-' in assembly name filter

--- run-coverage.sh
+++ run-coverage.sh
@@ -11,8 +11,10 @@ cd .coverage
 
 lcov -o lcov.info -a core.info -t core \
     -a glib-2.0.info -t glib-2.0 \
-    -a girepository-2.0.info -t girepository-2.0 \
+    -a gobject-2.0.info -t gobject-2.0 \
     -a gio-2.0.info -t gio-2.0 \
+    -a girepository-2.0.info -t girepository-2.0 \
+    -a gmodule-2.0.info -t gmodule-2.0 \
     -a gtk-4.0.info -t gtk-4.0 \
 
 if [ "$TRAVIS" != "true" ]; then
