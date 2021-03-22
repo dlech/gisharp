b311dfd70407d01e68aa40d4a3d30e5f61b157bf
David Lechner <david@lechnology.com>
2021-03-21 20:48:27 -0500

    GObject: add TypeDefaultInterface class

---
+++ src/Lib/GObject-2.0/TypeDefaultInterface.cs
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2021 David Lechner <david@lechnology.com>
+
+using System;
+using System.ComponentModel;
+using GISharp.Runtime;
+
+namespace GISharp.Lib.GObject
+{
+    unsafe partial class TypeDefaultInterface
+    {
+        /// <summary>
+        /// For internal runtime use only.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public TypeDefaultInterface(IntPtr handle, Transfer ownership) : base(handle, ownership)
+        {
+            if (ownership != Transfer.Full) {
+                throw new NotSupportedException();
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void Dispose(bool disposing)
+        {
+            if (handle != IntPtr.Zero) {
+                g_type_default_interface_unref((TypeInterface.UnmanagedStruct*)handle);
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
