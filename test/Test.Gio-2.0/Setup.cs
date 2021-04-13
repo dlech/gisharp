ecfe2af15e418091a4411bd048731bfc7488f3ad
David Lechner <david@lechnology.com>
2021-04-12 23:17:28 -0500

    test: ignore CA1050

--- test/Test.Gio-2.0/Setup.cs
+++ test/Test.Gio-2.0/Setup.cs
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2015-2019 David Lechner <david@lechnology.com>
 
-
 // no namespace, so that this applies to all tests in the assembly
-
+#pragma warning disable CA1050
 public class Setup : GISharp.Test.Setup
 {
 }
+#pragma warning restore CA1050
