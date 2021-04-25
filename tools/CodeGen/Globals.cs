e4d82ad4bc221fb658ad61c4efea257c6b47aa41
David Lechner <david@lechnology.com>
2021-04-25 18:31:00 -0500

    remove unused code

--- tools/CodeGen/Globals.cs
+++ tools/CodeGen/Globals.cs
@@ -28,52 +28,6 @@ namespace GISharp.CodeGen
         /// </summary>
         public const string GISharpNamespace = "http://gisharp.org/introspection/gisharp/1.0";
 
-        /// <summary>
-        /// Gets the interfaces implemented by a type in a safe way.
-        /// </summary>
-        /// <returns>The list of interfaces.</returns>
-        /// <param name="type">Type.</param>
-        /// <remarks>
-        /// This is needed because we use MakeGeneric to create generics that
-        /// use GirType as a type parameter. If we do this, internallally
-        /// the implementation throws a NotSupportedException when calling
-        /// GetInterfaces. We get around this by getting the generic type
-        /// that it was created from.
-        /// </remarks>
-        public static Type[] SafeGetInterfaces(this Type type)
-        {
-            try {
-                return type.GetInterfaces();
-            }
-            catch (NotSupportedException) {
-                if (type.IsGenericType) {
-                    var genericType = type.GetGenericTypeDefinition();
-                    return genericType.GetInterfaces();
-                }
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// Because of the dynamic types used in CodeGen, we have to try really
-        /// hard to determine inheritance.
-        /// </summary>
-        public static bool IsSubclassOf<T>(this Type type)
-        {
-            if (type is null) {
-                throw new ArgumentNullException(nameof(type));
-            }
-            try {
-                if (type.IsConstructedGenericType) {
-                    return type.GetGenericTypeDefinition().IsSubclassOf<T>();
-                }
-                return type.IsSubclassOf(typeof(T));
-            }
-            catch (NotSupportedException) {
-                return typeof(T).IsAssignableFrom(type);
-            }
-        }
-
         public static bool EnableDebugLogging { get; set; }
 
         public static ILoggerFactory LoggerFactory => Microsoft.Extensions.Logging.LoggerFactory.Create(builder => {
